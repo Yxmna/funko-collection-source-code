0515a058940bd6ae482c7e066cc4fde9b827c91d
Yomna <57047086+Yxmna@users.noreply.github.com>
2022-05-12 15:38:47 +0200

    Add imgs

--- src/components/Collection.jsx
+++ src/components/Collection.jsx
@@ -2,39 +2,44 @@ import React, { useState, useEffect } from "react";
 import Pop from "../components/Pop";
 
 function Collection(props) {
-
   let pops = props.pops;
 
   pops.sort(function (a, b) {
-    return (b.value - a.value);
-  })
+    return b.value - a.value;
+  });
 
-  return(
+  return (
     <article className="collection">
       <h1>{props.collection.name}</h1>
       <p>{props.collection.description}</p>
       <ul className="pop_achieved">
-        {
-          pops.filter(pop => pop.ok).map(pop => (
-
-            <li  key={pop.name+"-"+pop.number+"-"+pop.features?.join("-")} className={pop.ok?"":"wanted"}> <Pop pop={pop} /> </li>
-
-          ))
-        }
-              </ul>
-              <ul className="pop_missing"> 
-        {
-          pops.filter(pop => !pop.ok).map(pop => (
-
-            <li  key={pop.name+"-"+pop.number+"-"+pop.features?.join("-")} className={pop.ok?"":"wanted"}> <Pop pop={pop} /> </li>
-
-          ))
-        }
-
+        {pops
+          .filter((pop) => pop.ok)
+          .map((pop) => (
+            <li
+              key={pop.name + "-" + pop.number + "-" + pop.features?.join("-")}
+              className={pop.ok ? "" : "wanted"}
+            >
+              {" "}
+              <Pop pop={pop} />{" "}
+            </li>
+          ))}
+      </ul>
+      <ul className="pop_missing">
+        {pops
+          .filter((pop) => !pop.ok)
+          .map((pop) => (
+            <li
+              key={pop.name + "-" + pop.number + "-" + pop.features?.join("-")}
+              className={pop.ok ? "" : "wanted"}
+            >
+              {" "}
+              <Pop pop={pop} />{" "}
+            </li>
+          ))}
       </ul>
     </article>
-  )
-
+  );
 }
 
 export default Collection;
